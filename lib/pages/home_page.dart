6b75de9385c1521c252e84ec21291434f7f48cce
JulieNonnon <julie.nonnon@gmail.com>
2023-12-17 00:46:24 +0100

    Feat/basic structure for home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_examen1/components/video_hero.dart';
+import 'package:flutter_examen1/pages/regions_page.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key, required this.title});
@@ -18,14 +20,67 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: ListView( // agencement des sections du body en colonne
-        children: const <Widget>[ // sections du body
-          //... ici empilez les differentes sections du body.
-          //... séparez les avec du padding
-          VideoHero(), // component affichant un Hero avec un background Video
 
+      body: ListView(
+        children: <Widget>[ // Ajout des crochets pour entourer les enfants
+          const VideoHero(), // component affichant un Hero avec un background Video
+          const Text(
+            'I. Nos Régions',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              backgroundColor: Colors.blue,
+            ),
+          ),
+          Container(
+            color: const Color.fromARGB(255, 224, 224, 224),
+            padding: const EdgeInsets.all(12.0),
+            width: double.maxFinite,
+            child: Column( // Utilisation d'un Column pour agencer les enfants en colonne
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                  //... ici empilez les differentes sections du body.
+                //... séparez les avec du padding
+              
+                const Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim.',
+                  style: TextStyle(
+                    fontSize: 12.0,
+                    color: Colors.black,
+                  ),
+                ),
+
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder:(context)=>
+                      const RegionPage(title:"Region Page"))
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey,
+                    foregroundColor: Colors.black,
+                  ),
+                  child: const Text(
+                    "Rechercher Par Région",
+                  ),
+                ),
+
+                const Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim.',
+                  style: TextStyle(
+                    fontSize: 10.0,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
+
     );
   }
 }
